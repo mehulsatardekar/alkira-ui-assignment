8ac2c7207363c22d4fee4c2880b28145fa5b0221
mehulsatardekar <satardekar042197@gmail.com>
2023-01-06 21:51:52 +0530

    fix : load pageination component after data is being loaded

--- src/components/pagination/PaginationList.tsx
+++ src/components/pagination/PaginationList.tsx
@@ -19,65 +19,69 @@ const PaginationList = () => {
 
 
     return (
-        <div className="pagination-container">
-            <Pagination size="sm" className="btn-arrows">
-                <div className="select-container">
-                    <span>Show</span>
-                    <select
-                        name="page"
-                        onClick={(e) => {
-                            dispatchModifiers({ type: "CURRENT_PAGE", payload: 1 });
-                            dispatchModifiers({ type: "TEAMS_PER_PAGE", payload: Number(e.currentTarget.value) });
-                        }}
-                    >
-                        <option value="10">10</option>
-                        <option value="20">20</option>
-                        <option value="30">30</option>
-                    </select>
-                    <span>Entries</span>
-                </div>
-                <div>
-                    <span
-                        className={`${modifierState.currentPage === 1 ? "fade" : "arrow"}`}
-                        onClick={() => {
-                            dispatchModifiers({
-                                type: "CURRENT_PAGE",
-                                payload: modifierState.currentPage > 1 ? modifierState.currentPage - 1 : 1,
-                            });
-                        }}
-                    >
-                        <span title="go back">{"<"}</span>
-                    </span>
-                </div>
-                {pageNumberList.map((number) => {
-                    return (
-                        <PageItem
-                            key={number}
-                            active={number === modifierState.currentPage}
-                            onClick={() => {
-                                gotoNextPage(number);
-                            }}
-                        >
-                            {number}
-                        </PageItem>
-                    );
-                })}
+        <>
+            {
+                !modifierState.isLoading && <div className="pagination-container">
+                    <Pagination size="sm" className="btn-arrows">
+                        <div className="select-container">
+                            <span>Show</span>
+                            <select
+                                name="page"
+                                onClick={(e) => {
+                                    dispatchModifiers({ type: "CURRENT_PAGE", payload: 1 });
+                                    dispatchModifiers({ type: "TEAMS_PER_PAGE", payload: Number(e.currentTarget.value) });
+                                }}
+                            >
+                                <option value="10">10</option>
+                                <option value="20">20</option>
+                                <option value="30">30</option>
+                            </select>
+                            <span>Entries</span>
+                        </div>
+                        <div>
+                            <span
+                                className={`${modifierState.currentPage === 1 ? "fade" : "arrow"}`}
+                                onClick={() => {
+                                    dispatchModifiers({
+                                        type: "CURRENT_PAGE",
+                                        payload: modifierState.currentPage > 1 ? modifierState.currentPage - 1 : 1,
+                                    });
+                                }}
+                            >
+                                <span title="go back">{"<"}</span>
+                            </span>
+                        </div>
+                        {pageNumberList.map((number) => {
+                            return (
+                                <PageItem
+                                    key={number}
+                                    active={number === modifierState.currentPage}
+                                    onClick={() => {
+                                        gotoNextPage(number);
+                                    }}
+                                >
+                                    {number}
+                                </PageItem>
+                            );
+                        })}
 
-                <div>
-                    <span
-                        className={`${modifierState.currentPage === pageNumberList.length ? "fade" : "arrow"}`}
-                        onClick={() => {
-                            dispatchModifiers({
-                                type: "CURRENT_PAGE",
-                                payload: modifierState.currentPage < pageNumberList.length ? modifierState.currentPage + 1 : pageNumberList.length,
-                            });
-                        }}
-                    >
-                        <span title="go forward">{">"}</span>
-                    </span>
+                        <div>
+                            <span
+                                className={`${modifierState.currentPage === pageNumberList.length ? "fade" : "arrow"}`}
+                                onClick={() => {
+                                    dispatchModifiers({
+                                        type: "CURRENT_PAGE",
+                                        payload: modifierState.currentPage < pageNumberList.length ? modifierState.currentPage + 1 : pageNumberList.length,
+                                    });
+                                }}
+                            >
+                                <span title="go forward">{">"}</span>
+                            </span>
+                        </div>
+                    </Pagination>
                 </div>
-            </Pagination>
-        </div>
+            }
+        </>
     )
 }
 
